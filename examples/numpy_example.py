190fed5dab33740023266a7eedebdde8501437be
Adam Sorensen <assassinadam@hotmail.com>
2017-01-24 15:14:09 -0700

    changed numpy.ndarray to numpy_ndarray

--- examples/numpy_example.py
+++ examples/numpy_example.py
@@ -10,21 +10,21 @@ celeryapp = celery.Celery(
 task = {
     'name': 'append_tables',
     'inputs': [
-        {'name': 'a', 'type': 'numpy', 'format': 'numpy.ndarray'},
-        {'name': 'b', 'type': 'numpy', 'format': 'numpy.ndarray'}
+        {'name': 'a', 'type': 'numpy', 'format': 'numpy_ndarray'},
+        {'name': 'b', 'type': 'numpy', 'format': 'numpy_ndarray'}
     ],
-    'outputs': [{'name': 'c', 'type': 'numpy', 'format': 'numpy.ndarray'}],
+    'outputs': [{'name': 'c', 'type': 'numpy', 'format': 'numpy_ndarray'}],
     'script': 'c = a + b',
     'mode': 'python'
 }
 
 async_result = celeryapp.send_task('girder_worker.run', [task], {
     'inputs': {
-        'a': {'format': 'numpy.ndarray', 'data': ([1, 2, 3], [4, 5, 6])},
-        'b': {'format': 'numpy.ndarray', 'data': ([2, 4, 6], [1, 3, 5])}
+        'a': {'format': 'numpy_ndarray', 'data': ([1, 2, 3], [4, 5, 6])},
+        'b': {'format': 'numpy_ndarray', 'data': ([2, 4, 6], [1, 3, 5])}
     },
     'outputs': {
-        'c': {'format': 'numpy.ndarray', 'uri': 'file://output.json'}
+        'c': {'format': 'numpy_ndarray', 'uri': 'file://output.json'}
     }
 })
 
