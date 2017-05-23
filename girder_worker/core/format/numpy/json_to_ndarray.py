96f8a9f62fe540dd69420775f46540a047eb5a46
Adam Sorensen <assassinadam@hotmail.com>
2017-05-23 12:58:29 -0600

    fixed some formating

--- girder_worker/core/format/numpy/json_to_ndarray.py
+++ girder_worker/core/format/numpy/json_to_ndarray.py
@@ -1,7 +1,3 @@
 import json
 import numpy as np
-#import pickle
-#output = json.loads(input)
-#output = pickle.loads(input)
 output = np.asarray(json.loads(input))
-#output = json.loads(np.asarray(input))
--- girder_worker/core/format/numpy/ndarray_to_json.py
+++ girder_worker/core/format/numpy/ndarray_to_json.py
@@ -1,7 +1,2 @@
 import json
-import numpy as np
-import pickle
-
-#output = pickle.dumps(input.tolist())
-#output = pickle.dumps(input, protocol=0)
 output = json.dumps(input.tolist())
